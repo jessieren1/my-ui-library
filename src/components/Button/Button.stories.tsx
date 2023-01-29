67cfd93684a3e383ecb294552836186eb74127b1
jessieren1 <jessieren0003@gmail.com>
2023-01-29 22:30:00 +1100

    feat: initial storybook for button component

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { Button, ButtonSize, ButtonType } from "./button";
+
+export default {
+  title: "Button Component",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+export const defaultButton: ComponentStory<typeof Button> = () => (
+  <Button onClick={action("clicked")}>Default button</Button>
+);
+
+export const buttonWithSize: ComponentStory<typeof Button> = () => (
+  <>
+    <Button size={ButtonSize.Large}>Large button</Button>
+    <Button size={ButtonSize.Small}>Small button</Button>
+  </>
+);
+
+export const buttonWithType: ComponentStory<typeof Button> = () => (
+  <>
+    <Button btnType={ButtonType.Primary}>Primary button</Button>
+    <Button btnType={ButtonType.Default}>Default Button</Button>
+    <Button btnType={ButtonType.Danger}>Danger button</Button>
+    <Button btnType={ButtonType.Link} href="https://google.com">
+      Link button
+    </Button>
+  </>
+);
